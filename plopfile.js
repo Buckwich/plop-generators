d5dba8dde6d5a1ae0b22943f6a60be28ff3f3dc9
Simon <github@buckwich.de>
2026-05-07 14:33:17 +0200

    fix: await loading generators

--- plopfile.js
+++ plopfile.js
@@ -1,26 +1,26 @@
-module.exports = function (plop) {
+module.exports = async function (plop) {
   // Load Angular generators
-  plop.load('./generators/angular/component');
+  await plop.load('./generators/angular/component/index.js');
 
   // Load React generators
-  plop.load('./generators/react/component');
-  plop.load('./generators/react/hook');
+  await plop.load('./generators/react/component');
+  await plop.load('./generators/react/hook');
 
   // Load Vue generators
-  plop.load('./generators/vue/component');
-  plop.load('./generators/vue/composable');
-  plop.load('./generators/vue/store');
+  await plop.load('./generators/vue/component');
+  await plop.load('./generators/vue/composable');
+  await plop.load('./generators/vue/store');
 
   // Load NestJS generators
-  plop.load('./generators/nestjs/module');
-  plop.load('./generators/nestjs/controller');
-  plop.load('./generators/nestjs/service');
-  plop.load('./generators/nestjs/resource');
-  plop.load('./generators/nestjs/entity');
-  plop.load('./generators/nestjs/gateway');
-  plop.load('./generators/nestjs/middleware');
-  plop.load('./generators/nestjs/guard');
-  plop.load('./generators/nestjs/interceptor');
-  plop.load('./generators/nestjs/pipe');
-  plop.load('./generators/nestjs/filter');
+  await plop.load('./generators/nestjs/module');
+  await plop.load('./generators/nestjs/controller');
+  await plop.load('./generators/nestjs/service');
+  await plop.load('./generators/nestjs/resource');
+  await plop.load('./generators/nestjs/entity');
+  await plop.load('./generators/nestjs/gateway');
+  await plop.load('./generators/nestjs/middleware');
+  await plop.load('./generators/nestjs/guard');
+  await plop.load('./generators/nestjs/interceptor');
+  await plop.load('./generators/nestjs/pipe');
+  await plop.load('./generators/nestjs/filter');
 };
